ed320c18559228f3d22906b483996a04708a0962
noshabawani <noshabawani@gmail.com>
2020-08-25 15:53:28 +0530

    connit 2

--- src/app/candidate/candidate.component.ts
+++ src/app/candidate/candidate.component.ts
@@ -305,4 +305,11 @@ export class CandidateComponent implements OnInit {
       }
     });
   }
+
+  myAgeFunction(dob){
+    var date = new Date(dob);
+        var ageDifMs = Date.now() - date.getTime();
+        var ageDate = new Date(ageDifMs); // miliseconds from epoch
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
 }
